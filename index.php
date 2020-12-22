90212ce3528701e43cf68b82d45f95a161e40362
bibisha-susan-mathew <bibishasm142@gmail.com>
2020-12-22 14:37:33 +0530

    web1 completed

--- index.php
+++ index.php
@@ -9,20 +9,20 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-secondary">
         <div class="container-fluid">
-          <a class="navbar-brand" href="index.html">MUSIC ACADEMY</a>
+          <a class="navbar-brand" href="index.php">MUSIC ACADEMY</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="studreg.html">STUDENT REGISTRATION</a>
+                <a class="nav-link active" aria-current="page" href="studreg.php">STUDENT REGISTRATION</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="studlogin.html">STUDENT LOGIN</a>
+                <a class="nav-link" href="studlogin.php">STUDENT LOGIN</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="tchrlogin.html"> TEACHER LOGIN</a>
+                <a class="nav-link" href="tchrlogin.php"> TEACHER LOGIN</a>
               </li>
              
             
